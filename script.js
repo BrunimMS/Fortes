858837225bfe80b42f952203b628b9d1789ba0dc
BrunimMS <38635661+BrunimMS@users.noreply.github.com>
2023-11-09 16:08:41 -0300

    Backend Creation

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
-function menu(){
-    if (document.getElementById('menu').classList.contains('open')){
-        document.getElementById('menu').classList.remove('open');
-        document.getElementById('menuItens').classList.add('closed');
-        document.getElementById('header').classList.remove('headerOpen');
+const mysql = require("mysql");
+const express = require("express");
+const app = express();
+
+var con = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "123456",
+    database: "fortes"
+})
+con.connect((err) =>{
+    if (err){
+        console.log(err)
     } else {
-        document.getElementById('menu').classList.add('open');
-        document.getElementById('menuItens').classList.remove('closed');
-        document.getElementById('header').classList.add('headerOpen');
+        console.log("Database Connected!")
     }
-}
+})
+
+app.set("view-engine", 'handlebars');
+
+app.use(express.static('static'));
+
+app.get("/", (req, res) => {
+    var result = con.query("SELECT nome_colab FROM colaborador", (result, error) =>{
+        if (error){
+            console.log(error);
+        } else {
+            console.log(result);
+        }
+    })
+    res.render("index.hbs", {resultado: result});
+})
+app.get("/novoPedido", (req, res) => {
+    res.render("novoPedido.hbs");
+})
+app.get("/login", (req, res) => {
+    res.render("login.hbs");
+})
+app.get("/registro", (req, res) => {
+    res.render("cadastro.hbs");
+})
+
+app.listen(8080, () => {
+    console.log("Server Started!");
+});
